bed5a3236daa314eb2de670df4f80d63b3310109
Jaimie <162511163+JaimieHemmings@users.noreply.github.com>
2024-04-10 12:12:46 +0100

    Add files via upload

--- btr/settings.py
+++ btr/settings.py
@@ -106,3 +106,8 @@ EMAIL_PORT = '587'
 EMAIL_HOST_USER = '' #GMAIL USERNAME
 EMAIL_HOST_PASSWORD = '' #GMAIL PASSWORD
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
